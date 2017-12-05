b31a3843c4c994800cc03dc1a56bdfade9117ec3
mtoca <33788304+mtoca@users.noreply.github.com>
2017-12-05 11:44:24 -0500

    rm changes

--- ReplicaManager/src/ProcessHandler.java
+++ ReplicaManager/src/ProcessHandler.java
@@ -22,9 +22,25 @@ public class ProcessHandler {
 			if(status == true)  // For amanda implementation
 			{
 				replica.add(runtime.exec(new String[]{"java", replicaPath,"BC", ("" + (port+1))}));
+				String s = new String ("java" + replicaPath + "BC" + "" +(port+1));
 				replica.add(runtime.exec(new String[]{"java", replicaPath,"MB", ("" + (port+2))}));
+				String s2 = new String ("java" + replicaPath + "MB" + "" + (port+2));
 				replica.add(runtime.exec(new String[]{"java", replicaPath,"NB", ("" + (port+3))}));
+				String s3 = new String ("java" + replicaPath + "NB" + "" + (port+3));
 				replica.add(runtime.exec(new String[]{"java", replicaPath,"QC", ("" + (port+4))}));
+				String s4 = new String ("java" + replicaPath + "QC" + "" + (port+4));
+				
+			
+					Process p1 = replica.get(0);
+					Process p2 = replica.get(1);
+					Process p3 = replica.get(2);
+					Process p4 = replica.get(3);
+					p1 = runtime.exec(s);
+					p2 = runtime.exec(s2);
+					p3 = runtime.exec(s3);
+					p4 = runtime.exec(s4);
+					
+				
 			}
 			else  // Brandon implementation
 			{
@@ -32,12 +48,13 @@ public class ProcessHandler {
 				cmdarray[0] = replicaPath;
 				cmdarray[1] = "" + (port + 1);
 				replica.add(runtime.exec(replicaPath));
+				
+				for (int i=0; i<replica.size(); i++){
+					Process p = replica.get(i);
+					p = runtime.exec(replicaPath);
+				}
 			}
 			
-			for (int i=0; i<replica.size(); i++){
-				Process p = replica.get(i);
-				p = runtime.exec(replicaPath);
-			}
 			
 			resetVM();
 		} catch (IOException e) {
